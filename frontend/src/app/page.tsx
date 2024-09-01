4beaf9aa165ca72b20fc09719ec3cdae560cf4fc
Neva <hmtam110501@gmail.com>
2024-09-01 18:13:42 +0700

    intialize frontend app

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import PrimaryAppBar from "@/components/primary-app-bar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <PrimaryAppBar />
+    </main>
+  );
+}
